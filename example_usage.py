1fead3f5807a2331ea06f22990e5ee90edf51f10
Nick Williams <nwilliams@smithers.com>
2024-12-03 15:16:26 -0600

    remove sensitive info

--- example_usage.py
+++ example_usage.py
@@ -1,7 +1,7 @@
 import qbench
 
 def main():
-    qb = qbench.connect(base_url="https://origo.qbench.net", api_key="0c278123-168f-4611-b1da-99bc95963286", api_secret="545f4438-9b52-4e09-b032-d138c3fb7cac")
+    qb = qbench.connect(base_url="<yourdomainhere>", api_key="<yourapikeyhere>", api_secret="<yourapisecrethere>")
     filters = {
         "customer_account_id": "73"
     }
